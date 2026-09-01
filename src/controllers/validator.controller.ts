6db2934008f26f0baccb8d2b99b4f7dd2fffcde5
Michael Okoronkwo <michaelokoronkwo59@gmail.com>
2026-09-01 15:00:45 +0100

    added the service to the controller

--- src/controllers/validator.controller.ts
+++ src/controllers/validator.controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import { validateLuhn } from "../utils/luhn";
+import { CardService } from "../services/card.service";
+
+const cardService = new CardService();
 
 export const validateCard = (req: Request, res: Response) => {
   const { cardNumber } = req.body;
     if (!cardNumber) {
         return res.status(400).json({ error: "Card number is required" });
     }
-
-    const isValid = validateLuhn(cardNumber);
-    res.json({ valid: isValid });
+    if(typeof cardNumber !== "string") {
+        return res.status(400).json({ error: "Card number must be a string" });
+    }
+    const result = cardService.validate(cardNumber);
+    res.json(result);
 }
 
