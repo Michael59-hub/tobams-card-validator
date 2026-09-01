3c6116f84c9800b63aacb5f61c1fe6443495e1f4
Michael Okoronkwo <michaelokoronkwo59@gmail.com>
2026-09-01 13:32:43 +0100

    server running

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get("/", (req: any, res: any) => {
+  res.send("Server up");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);
+})
